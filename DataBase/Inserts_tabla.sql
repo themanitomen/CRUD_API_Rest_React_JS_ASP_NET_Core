e6962aee95ba81c2f02521f5a2cfc5a78aae29f3
Lopzu <mh_y_@outlook.es>
2022-11-07 20:26:55 -0600

    Se agregan scripts para BD SQL y espacio para proyecto de REACt

---
+++ DataBase/Inserts_tabla.sql
@@ -0,0 +1,10 @@
+INSERT INTO gestores_bd (Name, Launch, Developer) VALUES ('Microsoft SQL Server', 1989, 'Microsoft')
+GO
+INSERT INTO gestores_bd (Name, Launch, Developer) VALUES ('MySQL', 1995, 'Mysql AB, Sun Mycrosystems y Oracle Corporation')
+GO
+INSERT INTO gestores_bd (Name, Launch, Developer) VALUES ('PostgreSql', 1996, 'PostgreSQL Global')
+GO
+INSERT INTO gestores_bd (Name, Launch, Developer) VALUES ('Oracle Database', 1977, 'Oracle Corporation')
+GO
+INSERT INTO gestores_bd (Name, Launch, Developer) VALUES ('MongoDb', 2009, 'MongoDB inc.')
+GO
