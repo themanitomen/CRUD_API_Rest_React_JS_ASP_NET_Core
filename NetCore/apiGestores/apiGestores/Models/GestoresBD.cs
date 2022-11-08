69e2e05be5d27e05b56519130e24d7fe0fcb22a9
Lopzu <mh_y_@outlook.es>
2022-11-07 20:10:45 -0600

    Proyecto CRUD api net core agregado

---
+++ NetCore/apiGestores/apiGestores/Models/GestoresBD.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiGestores.Models
+{
+    public class GestoresBD
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        public int Launch { get; set; }
+        public string Developer { get;set; }
+
+    }
+}
